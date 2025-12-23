ecf0b18f5ef775e6e20f04b0fea0d4fed8920551
Noa Resare <noa.resare@portswigger.net>
2025-12-23 19:56:42 +0000

    minimal cleanup

--- src/flask_lab/app.py
+++ src/flask_lab/app.py
@@ -43,10 +43,8 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         super().__init__()
 
     def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
+        for key, value in self.options.items():
+            self.cfg.set(key, value)
 
     def load(self):
         return self.application
